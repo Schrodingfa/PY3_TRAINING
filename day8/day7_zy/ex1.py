c6e2c9bd71e72666b0277d0910351bf622ca3355
Schrodingfa <82779721@qq.com>
2020-05-27 17:16:04 +0800

    day7 ex

---
+++ day8/day7_zy/ex1.py
@@ -0,0 +1,16 @@
+# Author:jxy
+# 自定义列表的排序函数
+# 提示 需要返回值吗？
+# 不需要返回值 画内存图可以看出
+
+def sort(list01):
+    for i in range(len(list01)):
+        for j in range(i + 1, len(list01)):
+            if list01[i] > list01[j]:
+                list01[i], list01[j] = list01[j], list01[i]
+
+
+l01 = [1, 4, 7, 4, 8, 0, 6]
+
+sort(l01)
+print(l01)
---
+++ day8/day7_zy/ex2.py
@@ -0,0 +1,11 @@
+# Author:jxy
+# 英文单词反转
+# how are you --> you are how
+
+str01 = "how are you"
+str_s1 = str01.split()
+print(str_s1)
+str_s1.reverse()
+print(str_s1)
+str02 = " ".join(str_s1)
+print(str02)
